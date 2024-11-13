26d44082c80d55e8f9dcc0aa7636dc1de9d2291e
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-11-13 15:00:23 +0500

    add: adding some pages

--- src/components/InnerLayout/innerlayout.jsx
+++ src/components/InnerLayout/innerlayout.jsx
@@ -5,10 +5,8 @@ import SidebarInner from "./sidebar";
 function InnerLayoutSection({ children }) {
 	return (
 		<div className="flex h-screen bg-gray-100">
-			{/* Sidebar with fixed positioning */}
 			<SidebarInner className="w-64" />
 
-			{/* Main content area */}
 			<div className="flex-1 ml-64 flex flex-col overflow-y-hidden">
 				<HeaderInner />
 				<main className="p-4 flex-1 overflow-y-auto">{children}</main>
