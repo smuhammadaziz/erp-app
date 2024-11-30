0526ee80f3d3f794db716a2f3234c6198f69304e
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-11-30 11:17:52 +0500

    delete: removing comments from sales lahyout file

--- src/layout/SalesLayout/saleslayout.jsx
+++ src/layout/SalesLayout/saleslayout.jsx
@@ -11,20 +11,16 @@ function SalesPageLayoutMain({ children }) {
 				<header>
 					<SalesPageLayoutHeader />
 				</header>
-				{/* Main Content Area */}
 				<div className="flex flex-1">
-					{/* Content Area */}
 					<div className="flex-1 px-4 py-1 bg-white overflow-auto">
 						{children}
 					</div>
 
-					{/* Sidebar */}
 					<div className="w-52 bg-slate-100 text-white border-l-2">
 						<SalesPageLayoutSidebar />
 					</div>
 				</div>
 
-				{/* Footer */}
 				<div className="fixed bottom-0 left-0 w-full bg-white shadow-lg">
 					<SalesPageLayoutFooter />
 				</div>
