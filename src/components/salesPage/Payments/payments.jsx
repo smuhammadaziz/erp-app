33131200ccd6a58f30f333d00805f0e2478afc4c
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-11-30 11:09:16 +0500

    add: adding some changes

--- src/components/salesPage/Payments/payments.jsx
+++ src/components/salesPage/Payments/payments.jsx
@@ -6,7 +6,6 @@ import { HiOutlineCash } from "react-icons/hi";
 function SalesPagePaymentsSection() {
 	return (
 		<div className=" justify-center py-3">
-			{/* Cash Button */}
 			<button className="flex mb-5 items-center justify-center bg-green-100 hover:bg-green-200 text-green-600 hover:text-green-700 px-14 py-3 text-xl rounded-lg shadow-lg transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-green-300">
 				<HiOutlineCash className="mr-3 text-2xl" />
 				Cash
