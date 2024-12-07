bc3d36626896a20bdc5fd463ebe323e58ded2749
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-12-07 11:51:40 +0500

    add: adding main layout for products page

--- src/pages/products/products.jsx
+++ src/pages/products/products.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Layout } from "../../layout/Layout";
+import { Layout } from "../../layout/HomeLayout/layout";
 import InnerLayoutSection from "../../layout/InnerLayout/innerlayout";
 import ProductsPageComponent from "../../components/productPage/products/products";
 
