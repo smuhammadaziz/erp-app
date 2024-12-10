ee8d50910801349ec138a4be04830caf8db0cd80
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-12-10 17:54:09 +0500

    add: adding some changes for product table

--- src/components/productPage/products/ProductTable.jsx
+++ src/components/productPage/products/ProductTable.jsx
@@ -21,7 +21,7 @@ const ProductTable = ({ products, onViewProduct, onDeleteProduct }) => {
 								key={header}
 								className="px-6 py-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap"
 							>
-								{header}
+								{header} actions
 							</th>
 						))}
 					</tr>
