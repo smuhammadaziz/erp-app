846451b97b9baacea29a646b84edea0e003f15ee
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-12-20 16:36:39 +0500

    add: adding some changes

--- src/components/settingsPage/settings/LogoutButton.jsx
+++ src/components/settingsPage/settings/LogoutButton.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { FaSignOutAlt } from "react-icons/fa";
+import { MdOutlineChangeCircle } from "react-icons/md";
 
 function LogoutButton() {
 	return (
-		<button className="w-full flex items-center text-red-500 hover:bg-red-50 p-3 rounded-lg transition-colors">
-			<FaSignOutAlt className="mr-2 w-4 h-4" />
-			<span className="text-sm">Logout</span>
-		</button>
+		<div className="">
+			<button className="w-full flex items-center text-red-500 hover:bg-red-100 p-3 rounded-lg transition-colors">
+				<MdOutlineChangeCircle className="mr-2 w-4 h-4" />
+				<span className="text-sm">Foydalanuvchini o'zgartirish</span>
+			</button>
+			<button className="w-full bg-red-500 flex items-center mt-3 text-white hover:bg-transparent hover:text-red-500 border-2 border-red-500 p-3 rounded-lg transition-colors">
+				<FaSignOutAlt className="mr-2 w-4 h-4" />
+				<span className="text-sm">KSB-ID dan chiqish</span>
+			</button>
+		</div>
 	);
 }
 
