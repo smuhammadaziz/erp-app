0e702474537110a5841574d2aa8a04528fff1484
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-12-23 10:30:38 +0500

    add: adding some changes

--- src/components/settingsPage/settings/LogoutButton.jsx
+++ src/components/settingsPage/settings/LogoutButton.jsx
@@ -6,20 +6,20 @@ import { NavLink } from "react-router-dom";
 function LogoutButton() {
 	return (
 		<div className="">
-			<NavLink
+			{/* <NavLink
 				to="/login"
 				className="w-full flex items-center text-red-500 hover:bg-red-100 font-bold p-3 rounded-lg transition-colors"
 			>
 				<MdOutlineChangeCircle className="mr-2 w-4 h-4" />
 				<span className="text-sm">Foydalanuvchini o'zgartirish</span>
-			</NavLink>
-			{/* <NavLink
-				to="/intro"
+			</NavLink> */}
+			<NavLink
+				to="/login"
 				className="w-full bg-red-500 flex items-center mt-3 text-white font-bold hover:bg-transparent hover:text-red-500 border-2 border-red-500 p-3 rounded-lg transition-colors"
 			>
 				<FaSignOutAlt className="mr-2 w-4 h-4" />
-				<span className="text-sm ">KSB-ID dan chiqish</span>
-			</NavLink> */}
+				<span className="text-sm ">Hisobdan chiqish</span>
+			</NavLink>
 		</div>
 	);
 }
