814b910ecb3b7f8b3a35bca422fca8e9c0dc11a6
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-12-23 10:29:50 +0500

    add: adding some changes

--- src/components/settingsPage/settings/LogoutButton.jsx
+++ src/components/settingsPage/settings/LogoutButton.jsx
@@ -13,13 +13,13 @@ function LogoutButton() {
 				<MdOutlineChangeCircle className="mr-2 w-4 h-4" />
 				<span className="text-sm">Foydalanuvchini o'zgartirish</span>
 			</NavLink>
-			<NavLink
+			{/* <NavLink
 				to="/intro"
 				className="w-full bg-red-500 flex items-center mt-3 text-white font-bold hover:bg-transparent hover:text-red-500 border-2 border-red-500 p-3 rounded-lg transition-colors"
 			>
 				<FaSignOutAlt className="mr-2 w-4 h-4" />
 				<span className="text-sm ">KSB-ID dan chiqish</span>
-			</NavLink>
+			</NavLink> */}
 		</div>
 	);
 }
