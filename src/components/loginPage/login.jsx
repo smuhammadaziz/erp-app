0c11a8419fed2afdd6866a20f8789fc23ec6d797
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-12-23 10:11:49 +0500

    add: adding some changes

--- src/components/loginPage/login.jsx
+++ src/components/loginPage/login.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import { Layout } from "../../layout/Layout";
+import { Layout } from "../../layout/HomeLayout/layout";
 import { useNavigate } from "react-router-dom";
 import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
 import { Toaster, toast } from "sonner";
