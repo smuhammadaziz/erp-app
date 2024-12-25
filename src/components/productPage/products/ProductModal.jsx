cf306142cd79ddc157660fbbc801375a1dbad82f
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2024-12-25 15:58:00 +0500

    add: adding some changes

--- src/components/productPage/products/ProductModal.jsx
+++ src/components/productPage/products/ProductModal.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 
 const ProductModal = ({ isOpen, onClose, title, children }) => {
-	// Close the modal when clicking outside of the modal
 	useEffect(() => {
 		const handleOutsideClick = (e) => {
 			if (e.target === e.currentTarget) {
@@ -9,12 +8,10 @@ const ProductModal = ({ isOpen, onClose, title, children }) => {
 			}
 		};
 
-		// Add event listener on mount
 		if (isOpen) {
 			document.body.addEventListener("click", handleOutsideClick);
 		}
 
-		// Cleanup event listener on unmount or if modal closes
 		return () => {
 			document.body.removeEventListener("click", handleOutsideClick);
 		};
@@ -25,11 +22,11 @@ const ProductModal = ({ isOpen, onClose, title, children }) => {
 	return (
 		<div
 			className="fixed inset-0 z-[555] flex items-center justify-center bg-black bg-opacity-50"
-			onClick={onClose} // This will close the modal when clicking outside
+			onClick={onClose}
 		>
 			<div
 				className="bg-white rounded-lg shadow-lg w-full max-w-4xl max-h-[95vh] overflow-y-auto"
-				onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the modal
+				onClick={(e) => e.stopPropagation()}
 			>
 				<div className="p-6 flex justify-between items-center border-b border-gray-300">
 					<h2 className="text-3xl font-semibold text-gray-800">
