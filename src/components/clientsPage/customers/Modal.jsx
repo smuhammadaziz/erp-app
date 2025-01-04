a1e5891582838fe0119b781f9c866dee6286a282
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2025-01-04 11:18:15 +0500

    add: adding some changes for clicking outside the modal

--- src/components/clientsPage/customers/Modal.jsx
+++ src/components/clientsPage/customers/Modal.jsx
@@ -4,8 +4,17 @@ import { FaTimes as FaClose } from "react-icons/fa";
 const Modal = ({ isOpen, onClose, children, title }) => {
 	if (!isOpen) return null;
 
+	const handleOverlayClick = (e) => {
+		if (e.target === e.currentTarget) {
+			onClose();
+		}
+	};
+
 	return (
-		<div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+		<div
+			className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+			onClick={handleOverlayClick} // Close modal when clicking on overlay
+		>
 			<div className="bg-white rounded-lg p-6 w-full max-w-lg shadow-lg overflow-hidden">
 				<div className="flex justify-between items-center mb-4 border-b pb-2">
 					<h2 className="text-xl font-semibold text-gray-800 flex items-center space-x-2">
@@ -18,12 +27,11 @@ const Modal = ({ isOpen, onClose, children, title }) => {
 						<FaClose />
 					</button>
 				</div>
-				<div className="space-y-4">
-					{children}
-				</div>
+				<div className="space-y-4">{children}</div>
 			</div>
 		</div>
 	);
 };
 
 export default Modal;
+
