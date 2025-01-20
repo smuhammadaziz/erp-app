bc4ab56bb860f6acc26e9ec124dd0ce651d2ef8b
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2025-01-20 10:04:42 +0500

    add: adding some changes

--- src/components/salesPage/SalesSoldProducts/soldproducts.jsx
+++ src/components/salesPage/SalesSoldProducts/soldproducts.jsx
@@ -6,28 +6,6 @@ function SalesSoldProducts() {
 	const [products, setProducts] = useState([]);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(null);
-
-	// const handleDelete = async (id) => {
-	// 	try {
-	// 		const response = await fetch(
-	// 			`${nodeUrl}/api/delete/selling/${id}`,
-	// 			{
-	// 				method: "DELETE",
-	// 			},
-	// 		);
-
-	// 		if (!response.ok) {
-	// 			throw new Error("Failed to delete product");
-	// 		}
-
-	// 		setProducts(
-	// 			products.filter((product) => product.product_id !== id),
-	// 		);
-	// 	} catch (err) {
-	// 		setError(err.message);
-	// 	}
-	// };
-
 	const sales_id = localStorage.getItem("sales_id");
 
 	useEffect(() => {
@@ -40,26 +18,20 @@ function SalesSoldProducts() {
 					throw new Error("Failed to fetch products");
 				}
 				const data = await response.json();
-
 				setProducts(data[sales_id]?.products || []);
-				setError(null); // Reset the error flag
+				setError(null);
 				setLoading(false);
 			} catch (err) {
 				setLoading(false);
 			}
 		};
-
-		// Set up the interval to fetch data every 100ms
 		const intervalId = setInterval(fetchProducts, 1000);
-
-		// Clear the interval on component unmount
 		return () => clearInterval(intervalId);
 	}, [nodeUrl, sales_id]);
 
 	return (
 		<div className="py-1 h-[33vh]">
 			<div className="bg-white shadow-md rounded-lg h-full flex flex-col">
-				{/* Table */}
 				<div className="overflow-x-auto overflow-y-auto flex-grow">
 					<table className="min-w-full bg-white border border-gray-200">
 						<thead className="sticky z-0 top-0 bg-gray-100 shadow-sm z-10">
@@ -73,11 +45,8 @@ function SalesSoldProducts() {
 								<th className="py-2 px-5 border-b text-center w-[15%]">
 									Product Price
 								</th>
-								<th className="py-2 px-5 border-b text-center w-[10%]">
+								<th className="py-2 px-5 border-b text-center w-[15%]">
 									Total price
-								</th>
-								<th className="py-2 px-5 border-b text-center w-[10%]">
-									actions
 								</th>
 							</tr>
 						</thead>
@@ -85,7 +54,7 @@ function SalesSoldProducts() {
 							{error ? (
 								<tr>
 									<td
-										colSpan="5"
+										colSpan="4"
 										className="py-3 text-center text-red-500"
 									>
 										Error: {error}
@@ -94,7 +63,7 @@ function SalesSoldProducts() {
 							) : loading ? (
 								<tr>
 									<td
-										colSpan="5"
+										colSpan="4"
 										className="py-3 text-center text-gray-500"
 									>
 										Loading...
@@ -104,7 +73,7 @@ function SalesSoldProducts() {
 								products.map((product) => (
 									<tr
 										key={product.product_id}
-										className="text-gray-800 text-sm even:bg-gray-50"
+										className="text-gray-800 text-sm even:bg-gray-50 group relative hover:bg-gray-50"
 									>
 										<td
 											className="py-1 px-5 border-b text-left w-[15%]"
@@ -131,35 +100,30 @@ function SalesSoldProducts() {
 											)}
 										</td>
 										<td
-											className="py-1 px-5 border-b text-center w-[15%]"
+											className="py-1 px-5 border-b text-center w-[15%] relative"
 											title={product.summa}
 										>
-											{product.summa.toLocaleString(
-												"ru-RU",
-												{
-													minimumFractionDigits: 2,
-													maximumFractionDigits: 2,
-												},
-											)}
-										</td>
-										<td className="py-1 px-5 border-b text-center w-[10%]">
-											<button
-												// onClick={() =>
-												// 	handleDelete(
-												// 		product.product_id,
-												// 	)
-												// }
-												className="flex items-center justify-center bg-red-500 text-white p-1 rounded-md px-2 hover:bg-red-700 focus:outline-none"
-											>
-												<FiTrash2 />
-											</button>
+											<span>
+												{product.summa.toLocaleString(
+													"ru-RU",
+													{
+														minimumFractionDigits: 2,
+														maximumFractionDigits: 2,
+													},
+												)}
+											</span>
+											<div className="absolute right-0 top-0 h-full flex items-center opacity-0 -translate-x-2 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300">
+												<button className="flex items-center justify-center bg-red-500 text-white p-1 rounded-md px-2 hover:bg-red-700 focus:outline-none mr-2">
+													<FiTrash2 />
+												</button>
+											</div>
 										</td>
 									</tr>
 								))
 							) : (
 								<tr>
 									<td
-										colSpan="5"
+										colSpan="4"
 										className="py-3 text-center text-gray-500"
 									>
 										Hozircha mahsulotlar yo'q
