621614bf7a3d432b4ae72d1de33e1dd9f230970c
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2025-01-22 00:10:40 +0500

    add: adding some changes

--- src/layout/SalesLayout/PaymentModal.jsx
+++ src/layout/SalesLayout/PaymentModal.jsx
@@ -13,14 +13,16 @@ const PaymentModal = ({ isOpen, onClose, totalAmount = 47500.0 }) => {
 	if (!isOpen) return null;
 
 	return (
-		<div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4">
+		<div className="fixed inset-0 bg-black bg-opacity-50 text-black flex items-center justify-center p-4 z-[50]">
 			<div className="bg-white rounded-lg w-full max-w-2xl shadow-xl">
 				{/* Header */}
 				<div className="p-3 border-b flex justify-between items-center">
-					<h2 className="text-lg font-semibold">Payment</h2>
+					<h2 className="text-lg text-black font-semibold">
+						Payment
+					</h2>
 					<button
 						onClick={onClose}
-						className="p-1 hover:bg-gray-100 rounded-full transition duration-200"
+						className="p-1 rounded-full transition duration-200"
 					>
 						<IoClose className="w-5 h-5" />
 					</button>
