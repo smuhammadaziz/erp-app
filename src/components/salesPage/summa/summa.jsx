55699e105c68e48972a97c8e266a8f0e4d9bad34
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2025-01-21 10:52:01 +0500

    add: adding some changes

--- src/components/salesPage/summa/summa.jsx
+++ src/components/salesPage/summa/summa.jsx
@@ -52,8 +52,12 @@ function SalespageSummaSection() {
 				<HiOutlineCash className="mr-2 text-xl" />
 				{formatNumber(price)} so'm
 			</p>
+			<p className="flex items-center w-[200px] px-3 py-2 text-md rounded-md border-2 border-gray-300 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-300 transition mb-4">
+				<HiOutlineCash className="mr-2 text-xl" />
+				{formatNumber(price)} so'm
+			</p>
 
-			<div className="flex items-center w-[200px] mb-4">
+			{/* <div className="flex items-center w-[200px] mb-4">
 				<FaPercentage className="mr-2 text-xl" />
 				<input
 					type="number"
@@ -62,7 +66,7 @@ function SalespageSummaSection() {
 					className="px-2 py-2 border rounded-md w-full"
 					placeholder="Discount %"
 				/>
-			</div>
+			</div> */}
 
 			<p className="flex items-center w-[200px] px-3 py-2 text-md rounded-md border-2 border-gray-300 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-300 transition">
 				<HiOutlineCreditCard className="mr-2 text-xl" />
