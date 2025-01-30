4d23617ccccfa808cf0930bf661eccaa3b06bd9d
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2025-01-30 13:54:04 +0500

    add: adding some changes

--- src/components/loginPage/components/UserTypeDropdown.jsx
+++ src/components/loginPage/components/UserTypeDropdown.jsx
@@ -35,8 +35,11 @@ function UserTypeDropdown({
 						.includes(selectedUserType.toLowerCase()),
 				),
 			);
+			if (highlightedIndex === -1) {
+				setHighlightedIndex(0); // Set the first item active only when no selection is made
+			}
 		}
-	}, [selectedUserType, isDropdownOpen, users]);
+	}, [selectedUserType, isDropdownOpen, users, highlightedIndex]);
 
 	const handleSelection = (usertype) => {
 		setSelectedUserType(usertype);
@@ -77,11 +80,11 @@ function UserTypeDropdown({
 		}
 	};
 
-	console.log(selectedUserType);
+	console.log(filteredUsers);
 
 	return (
 		<div className="mb-6">
-			<label className="block text-xl font-medium text-gray-700 mb-2">
+			<label className="block text-xl text-gray-700 mb-2">
 				{content[language].login.select}
 			</label>
 			<div className="relative">
@@ -92,7 +95,7 @@ function UserTypeDropdown({
 					onFocus={handleInputFocus}
 					onChange={handleInputChange}
 					onKeyDown={handleKeyDown}
-					className="w-full px-4 py-4 bg-white border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+					className="w-full px-4 py-4 bg-white border font-semibold text-xl  rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
 					placeholder=""
 				/>
 				<div
@@ -109,9 +112,9 @@ function UserTypeDropdown({
 							<button
 								key={index}
 								onClick={() => handleSelection(user.usertype)}
-								className={`block w-full px-4 py-2 text-left hover:bg-gray-100 focus:outline-none ${
+								className={`block w-full px-4 py-2 text-left hover:bg-blue-300 focus:outline-none ${
 									index === highlightedIndex
-										? "bg-gray-100"
+										? "bg-blue-300"
 										: ""
 								}`}
 							>
