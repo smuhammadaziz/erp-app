84627f93187117e4abf8b85ca61fe42b5456728a
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2025-02-03 00:07:09 +0500

    add: adding some changes

--- src/components/salesPage/summa/summa.jsx
+++ src/components/salesPage/summa/summa.jsx
@@ -23,6 +23,9 @@ function SalespageSummaSection() {
 				const data = await response.json();
 
 				setPrice(parseFloat(data[sales_id].summa));
+				setDiscount(parseFloat(data[sales_id].discount));
+
+				setFinalPrice(data[sales_id].summa - data[sales_id].discount);
 			} catch (err) {
 				console.log(err);
 			}
@@ -34,7 +37,7 @@ function SalespageSummaSection() {
 	return (
 		<div className="items-center py-1">
 			<p
-				className={`flex items-center w-[200px] px-3 py-2 rounded-md border-2 border-gray-300 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-300 transition mb-4`}
+				className={`flex items-center w-[200px] font-bold px-3 py-2 rounded-md border-2 border-gray-300 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-300 transition mb-4`}
 			>
 				<HiOutlineCash className="mr-2 text-2xl" />
 				<span
@@ -51,17 +54,24 @@ function SalespageSummaSection() {
 					{price.toLocaleString("ru-RU", {
 						minimumFractionDigits: 2,
 						maximumFractionDigits: 2,
-					})}{" "}
-					so'm
+					})}
 				</span>
 			</p>
 
-			<p className="flex items-center w-[200px] px-3 py-2 text-md rounded-md border-2 border-gray-300 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-300 transition mb-4">
-				<HiOutlineCash className="mr-2 text-2xl" />0 %
+			<p className="flex items-center w-[200px] font-bold px-3 py-2 text-md rounded-md border-2 border-gray-300 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-300 transition mb-4">
+				<FaPercentage className="mr-2 text-2xl" />{" "}
+				{discount?.toLocaleString("ru-RU", {
+					minimumFractionDigits: 2,
+					maximumFractionDigits: 2,
+				}) || "0,00"}
 			</p>
 
-			<p className="flex items-center w-[200px] px-3 py-2 text-md rounded-md border-2 border-gray-300 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-300 transition">
-				<HiOutlineCreditCard className="mr-2 text-2xl" />
+			<p className="flex items-center font-bold w-[200px] px-3 py-2 text-md rounded-md border-2 border-gray-300 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-300 transition">
+				<HiOutlineCreditCard className="mr-2 text-2xl" />{" "}
+				{finalPrice?.toLocaleString("ru-RU", {
+					minimumFractionDigits: 2,
+					maximumFractionDigits: 2,
+				}) || "0,00"}
 			</p>
 		</div>
 	);
