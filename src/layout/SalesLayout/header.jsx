1573000e3f8d3fe3aebcb29eaa57382f7f91eb2b
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2025-02-02 14:27:36 +0500

    add: adding some changes

--- src/layout/SalesLayout/header.jsx
+++ src/layout/SalesLayout/header.jsx
@@ -201,7 +201,11 @@ function SalesPageLayoutHeader() {
 		return result || null;
 	}
 
-	// const foundObject = findObjectById(idToFind);
+	const today = new Date().toLocaleDateString("ru-RU", {
+		year: "numeric",
+		month: "2-digit",
+		day: "2-digit",
+	});
 
 	return (
 		<div className="salesfooter px-4 py-1 bg-slate-100 shadow-lg border-t border-gray-300 flex items-center justify-between">
@@ -247,7 +251,7 @@ function SalesPageLayoutHeader() {
 				</button>
 				<button className="flex items-center justify-center bg-blue-900 hover:bg-blue-950 text-slate-100 px-7 py-2 text-md rounded-lg shadow-lg transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-950">
 					<MdCalendarToday className="mr-3 text-xl" />
-					<span className="font-semibold">11.18.2024</span>
+					<span className="font-semibold">{today}</span>
 				</button>
 			</div>
 			{isListModalOpen && (
