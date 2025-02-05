5be30437e613bfca7445ff9028910169076e7474
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2025-02-05 10:45:38 +0500

    add: adding some changes

--- src/components/settingsPage/settings/LogoutButton.jsx
+++ src/components/settingsPage/settings/LogoutButton.jsx
@@ -12,7 +12,7 @@ function LogoutButton() {
 		<div className="">
 			<NavLink
 				to="/login"
-				className="w-full bg-red-500 flex items-center mt-3 text-white font-bold hover:bg-transparent hover:text-red-500 border-2 border-red-500 p-3 rounded-lg transition-colors"
+				className="w-full bg-red-500 flex items-center mt-3 text-white font-semibold hover:bg-transparent hover:text-red-500 border-2 border-red-500 p-3 rounded-lg transition-colors"
 			>
 				<FaSignOutAlt className="mr-2 w-4 h-4" />
 				<span className="text-sm ">
