3143c01a887116ccaa48a66b46aed965c17d7dd7
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2025-02-06 10:42:35 +0500

    add: adding currency for products table

--- src/components/productPage/products/ProductViewDetails.jsx
+++ src/components/productPage/products/ProductViewDetails.jsx
@@ -237,7 +237,7 @@ const ProductViewDetails = ({ product }) => {
 							{names[item.type] || "Loading..."}
 						</td>
 						<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-							{item.sale}
+							{item.sale} {currencyName}
 						</td>
 					</tr>
 				))}
