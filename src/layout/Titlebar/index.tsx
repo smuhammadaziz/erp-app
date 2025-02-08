2552986500cdbb91a5f150314e411c44d408e5a3
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2025-02-08 09:41:17 +0500

    add: adding some changes

--- src/layout/Titlebar/index.tsx
+++ src/layout/Titlebar/index.tsx
@@ -94,10 +94,7 @@ export const Titlebar: FC = () => {
 								className="flex-1 bg-red-600 hover:bg-red-700 text-white flex items-center justify-center py-3 px-6 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 focus:outline-none focus:ring-2 focus:ring-red-400"
 							>
 								<ImExit className="mr-2 text-xl" />
-								{
-									content[language as string].salesPage
-										.footerExitYes
-								}
+								OK
 							</button>
 							<button
 								onClick={() => setIsExitModalOpen(false)}
