5b558d162c74facc1c8a7a831eb07adb04fcd718
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2025-02-10 09:40:26 +0500

    add: adding some changes to my first sync

--- src/components/homePage/UserSettings.jsx
+++ src/components/homePage/UserSettings.jsx
@@ -342,7 +342,7 @@ const DownloaderModal = () => {
 						</p>
 						<div className="flex items-center ">
 							<button
-								onClick={() => setDownloadStatus("completed")}
+								onClick={() => setDownloadStatus("idle")}
 								className="w-full mx-3  bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition"
 							>
 								{content[language].firstSync.tryAgain}
