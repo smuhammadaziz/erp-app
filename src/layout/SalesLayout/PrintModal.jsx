e5b72bc2ca9b6c0b9ff35786dc1024656c9c399f
smuhammmadaziz <sayhelloaziz@gmail.com>
2025-02-25 15:47:56 +0500

    add: adding the 1.2 seconds showing success modal

--- src/layout/SalesLayout/PrintModal.jsx
+++ src/layout/SalesLayout/PrintModal.jsx
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MdClear } from "react-icons/md";
 import SuccessModal from "./SuccessModal";
 
 function PrintingModal({ setPrintModal }) {
 	const [successModal, setSuccessModal] = useState(false);
+
+	const showSuccessModal = () => {
+		setSuccessModal(true);
+		setTimeout(() => setSuccessModal(false), 1200);
+	};
+
 	return (
 		<div className="fixed inset-0 flex items-center justify-center bg-black/30 backdrop-blur-xs z-[100]">
 			<div className="bg-white w-[500px] rounded-xl shadow-2xl relative">
@@ -22,7 +28,7 @@ function PrintingModal({ setPrintModal }) {
 					<p className="text-base text-black mb-4">Чек чиқариш</p>
 					<div className="flex justify-center mt-5">
 						<button
-							onClick={() => setSuccessModal(true)}
+							onClick={() => showSuccessModal()}
 							className="px-10 mx-5 py-2 bg-white border-2 border-black text-black text-lg font-medium rounded-lg hover:bg-slate-100 transform hover:scale-105 transition-all duration-200"
 						>
 							OK (10)
--- src/layout/SalesLayout/SuccessModal.jsx
+++ src/layout/SalesLayout/SuccessModal.jsx
@@ -5,10 +5,10 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 function SuccessModal({ setSuccessModal }) {
 	return (
-		<div className="fixed inset-0 flex items-center justify-center bg-black/30 backdrop-blur-xs z-[100]">
-			<div className="bg-white w-[500px] h-[500px] rounded-xl shadow-2xl relative">
+		<div className="fixed inset-0 flex items-center justify-center  z-[100]">
+			<div className="bg-transparent w-[400px] h-[300px]  relative">
 				<div className="p-6">
-					<p className="text-base text-black mb-4">
+					<p className="text-base text-black mb-4 justify-center">
 						<DotLottieReact
 							src="https://lottie.host/fc406ebe-b131-43cf-99c1-36e8a4361abd/VnuvL768sW.lottie"
 							// loop
