5119d89078f2cf5706d575d1e5084313ca3b869b
smuhammmadaziz <sayhelloaziz@gmail.com>
2025-02-25 15:24:42 +0500

    add: adding dotlitte package and

--- src/layout/SalesLayout/PrintModal.jsx
+++ src/layout/SalesLayout/PrintModal.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdClear } from "react-icons/md";
+import SuccessModal from "./SuccessModal";
 
 function PrintingModal({ setPrintModal }) {
+	const [successModal, setSuccessModal] = useState(false);
 	return (
 		<div className="fixed inset-0 flex items-center justify-center bg-black/30 backdrop-blur-xs z-[100]">
 			<div className="bg-white w-[500px] rounded-xl shadow-2xl relative">
@@ -20,7 +22,7 @@ function PrintingModal({ setPrintModal }) {
 					<p className="text-base text-black mb-4">Чек чиқариш</p>
 					<div className="flex justify-center mt-5">
 						<button
-							// onClick={() => setShowErrorModal(false)}
+							onClick={() => setSuccessModal(true)}
 							className="px-10 mx-5 py-2 bg-white border-2 border-black text-black text-lg font-medium rounded-lg hover:bg-slate-100 transform hover:scale-105 transition-all duration-200"
 						>
 							OK (10)
@@ -34,6 +36,8 @@ function PrintingModal({ setPrintModal }) {
 					</div>
 				</div>
 			</div>
+
+			{successModal && <SuccessModal setSuccessModal={setSuccessModal} />}
 		</div>
 	);
 }
--- src/layout/SalesLayout/SuccessModal.jsx
+++ src/layout/SalesLayout/SuccessModal.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { MdClear } from "react-icons/md";
 
-function SuccessModal() {
+import tickIcon from "../../assets/tick.json";
+
+function SuccessModal({ setSuccessModal }) {
 	return (
 		<div className="fixed inset-0 flex items-center justify-center bg-black/30 backdrop-blur-xs z-[100]">
-			<div className="bg-white w-[500px] rounded-xl shadow-2xl relative">
+			<div className="bg-white w-[500px] h-[500px] rounded-xl shadow-2xl relative">
 				<div className="p-6">
 					<div className="flex justify-between items-center mb-4">
 						<h2 className="text-xl font-bold text-gray-800">
-							Печать
+							Success Modal
 						</h2>
 						<button
-							// onClick={() => setPrintModal(false)}
+							onClick={() => setSuccessModal(false)}
 							className="p-1.5 hover:bg-gray-100 rounded-full transition-colors duration-200"
 						>
 							<MdClear size={24} className="text-gray-500" />
 						</button>
 					</div>
-					<p className="text-base text-black mb-4">Чек чиқариш</p>
+					<p className="text-base text-black mb-4">
+						<tickIcon />
+					</p>
 					<div className="flex justify-center mt-5">
 						<button
 							// onClick={() => setShowErrorModal(false)}
