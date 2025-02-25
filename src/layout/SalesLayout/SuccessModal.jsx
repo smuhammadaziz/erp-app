f39cc19fadf115bb6f12ab1f860eb4ece31d4899
smuhammmadaziz <sayhelloaziz@gmail.com>
2025-02-25 15:37:21 +0500

    add: adding lottie icon

--- src/layout/SalesLayout/SuccessModal.jsx
+++ src/layout/SalesLayout/SuccessModal.jsx
@@ -11,7 +11,7 @@ function SuccessModal({ setSuccessModal }) {
 					<p className="text-base text-black mb-4">
 						<DotLottieReact
 							src="https://lottie.host/fc406ebe-b131-43cf-99c1-36e8a4361abd/VnuvL768sW.lottie"
-							// loop
+							loop
 							autoplay
 						/>
 					</p>
