05daf94ce9ecedd23d7b4720b720b9cefca5d0c6
smuhammmadaziz <sayhelloaziz@gmail.com>
2025-02-26 16:22:24 +0500

    add: adding some changes

--- src/layout/SalesLayout/SuccessModal.jsx
+++ src/layout/SalesLayout/SuccessModal.jsx
@@ -5,20 +5,13 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 function SuccessModal() {
 	const [status, setStatus] = useState("loading");
 
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setStatus("success");
-		}, 2000);
-
-		return () => clearTimeout(timer);
-	}, []);
-
 	return (
 		<div className="fixed inset-0 flex items-center justify-center bg-black/30 backdrop-blur-xs z-[100]">
-			<div className="bg-transparent w-[450px] h-[300px] relative flex items-center justify-center">
-				<div className="text-black text-4xl animate-spin">
-					<FiLoader />
-				</div>
+			<div className="bg-transparent w-[350px] h-[300px] relative flex items-center justify-center">
+				<DotLottieReact
+					src="https://lottie.host/fc406ebe-b131-43cf-99c1-36e8a4361abd/VnuvL768sW.lottie"
+					autoplay
+				/>
 			</div>
 		</div>
 	);
