1406f1744fa4dddd7cacc282a30ba53166d7caac
smuhammmadaziz <sayhelloaziz@gmail.com>
2025-02-26 17:42:14 +0500

    add: adding some changes

--- src/layout/SalesLayout/ErrorModal.jsx
+++ src/layout/SalesLayout/ErrorModal.jsx
@@ -8,33 +8,25 @@ function ErrorModal({ setPrintModal }) {
 		<div className="fixed inset-0 flex items-center justify-center bg-black/30 backdrop-blur-xs z-[100]">
 			<div className="bg-white w-[500px] rounded-xl shadow-2xl relative">
 				<div className="p-6">
-					<div className="flex justify-between items-center mb-4">
-						<h2 className="text-xl font-bold text-gray-800">
-							<IoIosWarning />
-						</h2>
-						<button
-							onClick={() => setPrintModal(false)}
-							className="p-1.5 hover:bg-gray-100 rounded-full transition-colors duration-200"
-						>
-							<TbAlertCircleFilled
-								size={24}
-								className="text-gray-500"
-							/>
-						</button>
-					</div>
-					<p className="text-base text-black mb-4">Xatolik</p>
+					<h2 className="text-6xl text-center justify-center flex font-bold text-orange-400">
+						<IoIosWarning size={140} />
+					</h2>
+
+					<p className="text-xl text-center text-black mb-4">
+						Савдони юборишда хатолик мавжуд!
+					</p>
 					<div className="flex justify-center mt-5">
 						<button
 							// onClick={() => setShowErrorModal(false)}
 							className="px-10 mx-5 py-2 bg-white border-2 border-black text-black text-lg font-medium rounded-lg hover:bg-slate-100 transform hover:scale-105 transition-all duration-200"
 						>
-							OK (10)
+							Қайта юбориш
 						</button>
 						<button
 							// onClick={() => setShowErrorModal(false)}
 							className="px-8 py-2 bg-red-500 text-white text-lg font-medium rounded-lg hover:bg-red-500 transform hover:scale-105 transition-all duration-200"
 						>
-							Отмена
+							Кейинроқ
 						</button>
 					</div>
 				</div>
