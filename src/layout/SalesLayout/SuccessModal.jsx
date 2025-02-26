43a2f5ba3ccaa84f33ea16f75897c9208f327002
smuhammmadaziz <sayhelloaziz@gmail.com>
2025-02-26 16:23:03 +0500

    add: addign some changes

--- src/layout/SalesLayout/SuccessModal.jsx
+++ src/layout/SalesLayout/SuccessModal.jsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { FiLoader } from "react-icons/fi";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 function SuccessModal() {
-	const [status, setStatus] = useState("loading");
-
 	return (
 		<div className="fixed inset-0 flex items-center justify-center bg-black/30 backdrop-blur-xs z-[100]">
 			<div className="bg-transparent w-[350px] h-[300px] relative flex items-center justify-center">
