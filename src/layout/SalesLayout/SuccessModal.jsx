a148bc6961f4aef973cab8c51143c25881df9bc8
smuhammmadaziz <sayhelloaziz@gmail.com>
2025-02-27 13:57:16 +0500

    add: adding some changes

--- src/layout/SalesLayout/SuccessModal.jsx
+++ src/layout/SalesLayout/SuccessModal.jsx
@@ -6,10 +6,6 @@ function SuccessModal() {
 	return (
 		<div className="fixed inset-0 flex items-center justify-center bg-black/30 backdrop-blur-xs z-[100]">
 			<div className="bg-transparent w-[350px] h-[300px] relative flex items-center justify-center">
-				{/* <DotLottieReact
-					src="https://lottie.host/fc406ebe-b131-43cf-99c1-36e8a4361abd/VnuvL768sW.lottie"
-					autoplay
-				/> */}
 				<img
 					src={successIcon}
 					alt=""
