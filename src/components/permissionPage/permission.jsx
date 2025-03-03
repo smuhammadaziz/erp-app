990fe99c691c0b893233e9019ff959f563df953b
smuhammmadaziz <sayhelloaziz@gmail.com>
2025-03-03 11:15:21 +0500

    add: enhance PermissionComponent with modal layout and buttons for user actions

--- src/components/permissionPage/permission.jsx
+++ src/components/permissionPage/permission.jsx
@@ -7,7 +7,115 @@ import useLang from "../../hooks/useLang";
 function PermissionComponent() {
 	const [language] = useLang("uz");
 
-	return <div>PermissionComponent</div>;
+	return (
+		<div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-[444]">
+			<div className="bg-white w-100 rounded-lg shadow-xl p-8 relative">
+				<div className="w-[500px]">
+					<TbHandStop className="text-6xl text-center text-blue-900 flex justify-center mx-auto mb-5" />
+					<h2 className="text-2xl font-semibold text-black mb-4 text-center">
+						Қурилмада чеклов мавжуд.
+					</h2>
+
+					<div className="flex items-center space-x-4">
+						<button
+							onClick={startDownload}
+							className="w-full bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition"
+						>
+							Қайтадан текшириш
+						</button>
+						<button
+							onClick={startDownload}
+							className="w-full bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition"
+						>
+							Дастурдан чиқиш
+						</button>
+					</div>
+				</div>
+
+				{/* {downloadStatus === "completed" && (
+					<div className="text-center w-[500px]">
+						<FaCheckCircle className="text-6xl text-center text-green-600 flex justify-center mx-auto mb-5" />
+						<h2 className="text-2xl font-semibold text-black mb-4">
+							{content[language].firstSync.syncComplete}
+						</h2>
+						<p className="text-black mb-6 text-lg">
+							{content[language].firstSync.dataSuccessfullySynced}
+						</p>
+						<button
+							onClick={() => {
+								closeModal();
+								handleUserSettings();
+								handleSetCurrency();
+								handleDeleteItems();
+							}}
+							className="w-full bg-green-600 text-white px-6 py-3 rounded-lg hover:bg-green-700 transition"
+						>
+							OK
+						</button>
+					</div>
+				)} */}
+
+				{/* {downloadStatus === "error" && (
+					<div className="text-center w-[500px]">
+						<MdErrorOutline className="text-red-500 text-6xl mb-6 flex justify-center mx-auto" />
+						<h2 className="text-2xl font-semibold text-gray-800 mb-4">
+							{content[language].firstSync.syncFailed}
+						</h2>
+						<p className="text-red-600 mb-6">
+							{
+								content[language].firstSync
+									.deviceAlreadyRegistered
+							}
+						</p>
+						<div className="flex items-center ">
+							<button
+								onClick={() => setDownloadStatus("idle")}
+								className="w-full mx-3  bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition"
+							>
+								{content[language].firstSync.tryAgain}
+							</button>
+							<NavLink
+								to="/intro"
+								className="w-full mx-3 bg-red-600 text-white px-6 py-3 rounded-lg hover:bg-red-700 transition"
+							>
+								{content[language].firstSync.goToKSB}
+							</NavLink>
+						</div>
+					</div>
+				)} */}
+
+				{/* {isNoInternetModalOpen && (
+					<div className="fixed inset-0 bg-black/40 backdrop-blur-sm flex items-center justify-center z-[790] p-4">
+						<div className="bg-white rounded-2xl max-w-md w-full shadow-2xl transform transition-all duration-300 ease-in-out scale-100 opacity-100 p-6 text-center">
+							<div className="mb-6">
+								<span className="text-center mx-auto justify-center block">
+									<MdOutlinePortableWifiOff
+										size={70}
+										className="text-center text-6xl mx-auto inline-block py-5"
+									/>
+								</span>
+								<h2 className="text-2xl font-bold text-gray-800 mb-2">
+									{content[language].firstSync.noInternet}
+								</h2>
+								<p className="text-gray-600 mb-4">
+									{content[language].firstSync.pleaseCheck}
+								</p>
+							</div>
+							<button
+								onClick={() => {
+									setIsNoInternetModalOpen(false);
+									setDownloadStatus("idle");
+								}}
+								className="w-full bg-blue-500 text-white py-3 rounded-xl hover:bg-blue-600 transition-colors duration-300 font-semibold"
+							>
+								{content[language].firstSync.tryAgain}
+							</button>
+						</div>
+					</div>
+				)} */}
+			</div>
+		</div>
+	);
 }
 
 export default PermissionComponent;
