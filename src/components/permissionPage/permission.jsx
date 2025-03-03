be30fd4b47819b4ca50dbfe49055de6a378b2a85
smuhammmadaziz <sayhelloaziz@gmail.com>
2025-03-03 11:04:48 +0500

    add: create PermissionComponent for handling permissions

---
+++ src/components/permissionPage/permission.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+import content from "../../localization/content";
+import useLang from "../../hooks/useLang";
+
+function PermissionComponent() {
+	const [language] = useLang("uz");
+	return <div>PermissionComponent</div>;
+}
+
+export default PermissionComponent;
+
