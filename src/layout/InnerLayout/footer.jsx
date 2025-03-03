d802771412acfd2e8047590a3e9745e3e29d50cf
smuhammmadaziz <sayhelloaziz@gmail.com>
2025-03-03 11:49:28 +0500

    feat: update InnerFooter to display support information and deadline notice

--- src/layout/InnerLayout/footer.jsx
+++ src/layout/InnerLayout/footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function InnerFooter() {
-	return <div>InnerFooter</div>;
+	return (
+		<div>
+			Texnik qo'llab quvvatlash (ITS) muddati tugashiga 10 kun qoldi.
+			Qo'shimcha ma'lumot uchun: +998 78 298 09 99
+		</div>
+	);
 }
 
 export default InnerFooter;
+
