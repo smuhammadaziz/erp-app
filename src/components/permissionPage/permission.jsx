b832468385284660f503a3b9f1aa6ae6e9419eb9
smuhammmadaziz <sayhelloaziz@gmail.com>
2025-03-03 11:11:37 +0500

    add: import TbHandStop icon in permission component

--- src/components/permissionPage/permission.jsx
+++ src/components/permissionPage/permission.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { TbHandStop } from "react-icons/tb";
 
 import content from "../../localization/content";
 import useLang from "../../hooks/useLang";
